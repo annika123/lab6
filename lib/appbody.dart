799d4be5bed3eb63aeb72f4756f0b87caa3802ca
annika123 <annika15-6045@diu.edu.bd>
2025-02-23 12:55:01 +0600

    Bottomnavication

---
+++ lib/appbody.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Appbody extends StatefulWidget {
+  const Appbody({super.key});
+
+  @override
+  State<Appbody> createState() => _AppbodyState();
+}
+
+class _AppbodyState extends State<Appbody> {
+  int currentIndex = 0;
+
+  var List<widget> _children = [
+    Text('Home'),
+    Text('Search'),
+    Text('Add'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorite',
+              backgroundColor: Colors.amber,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+              backgroundColor: Colors.amber,
+            ),
+          ],
+        ),
+    );
+  }
+}
