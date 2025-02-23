f28eb40c5f97869d41538a2ee8190ee955208325
annika123 <annika15-6045@diu.edu.bd>
2025-02-23 11:17:28 +0600

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyWidget());
+  runApp(const Myapp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +18,18 @@ class MyWidget extends StatelessWidget {
           backgroundColor: Colors.blue,
           centerTitle: true,
         ),
-        body: Center(
-          child: Text(
-            "Advanced UI & Navigation",
-            style: TextStyle(
-              fontSize: 30,
-            ),
+        body: SingleChildScrollView(
+          child: Column(
+          children:[
+            CachedNetworkImage(
+        imageUrl: "https://www.google.com/imgres?q=ai%20image%20hd&imgurl=https%3A%2F%2F4kwallpapers.com%2Fimages%2Fwallpapers%2Fbeautiful-woman-ai-art-3840x2160-11247.jpg&imgrefurl=https%3A%2F%2F4kwallpapers.com%2Ffantasy%2Fbeautiful-woman-ai-art-11247.html&docid=-bEibVbo2CBUFM&tbnid=Pe6RlVuw-IYOTM&vet=12ahUKEwiIrtKrgtmLAxUrzTgGHRWQMXAQM3oECFcQAA..i&w=3840&h=2160&hcb=2&ved=2ahUKEwiIrtKrgtmLAxUrzTgGHRWQMXAQM3oECFcQAA",
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+     ),
+          ],
           ),
         ),
-      ),
-
+    ),
     );
   }
 }
